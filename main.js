d5eab3e4da3c9952a04213e7d1a04eff0d0ee2fc
nugmara <dagmarag0@gmail.com>
2024-01-15 16:59:04 +0100

    Add addEventListener for touching screen

--- main.js
+++ main.js
@@ -21,6 +21,7 @@ const keys = document.querySelectorAll(".key")
 
 keys.forEach(key => key.addEventListener("transitionend", removeTransition))
 window.addEventListener("keydown", playSound)
+window.addEventListener("touchstart", playSound )
 
 
 
