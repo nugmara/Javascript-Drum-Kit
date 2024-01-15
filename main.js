765423d2233c4f35ab4979921fa2bf44082ad883
nugmara <dagmarag0@gmail.com>
2024-01-15 16:42:41 +0100

    add event listener for the transitions

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
 import './style.css'
 
-function playSound() {
-  
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+    console.log(e.keyCode)
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+
+    if (!audio) return
+
+    audio.currentTime = 0;
+    audio.play()
+    key.classList.add("playing")
 }
+
+
+window.addEventListener("keydown", playSound)
+
+
 
 
